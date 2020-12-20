11142b44d89d339916d0f423ef9684d7c106641b
JinseongHwang <eddy5360@naver.com>
2020-12-20 16:18:32 +0900

    refactor: 막대기를 알파벳으로 표현 방법 변경

--- 4_Hanoi/Hanoi.c
+++ 4_Hanoi/Hanoi.c
@@ -4,18 +4,18 @@ typedef signed char T;
 
 T subroutine(T height, T from, T tmp, T to) {
 	if (height == 1) {
-		printf("%d -> %d\n", from, to);
+		printf("%c -> %c\n", from, to);
 	}
 	else {
 		subroutine(height - 1, from, to, tmp);
-		printf("%d -> %d\n", from, to);
+		printf("%c -> %c\n", from, to);
 		subroutine(height - 1, tmp, from, to);
 	}
 }
 
 int main() {
-	// height는 3으로 고정, 막대기는 1, 2, 3으로 3개 고정
-	subroutine(3, 1, 2, 3);
+	// height는 3으로 고정, 막대기는 A, B, C로 3개 고정
+	subroutine(3, 'A', 'B', 'C');
 
 	return 0;
 }
