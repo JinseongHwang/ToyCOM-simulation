ac0ef9ed367fdb3c661fcd6e775e66d3f585d3ed
JinseongHwang <eddy5360@naver.com>
2020-12-20 23:26:57 +0900

    refactor: 두 기능을 함수화함

--- 2_Gugudan/Gugudan.c
+++ 2_Gugudan/Gugudan.c
@@ -2,7 +2,7 @@
 
 typedef signed char T;
 
-T subroutine() {
+T subroutine_1() {
 	T sum = 0;
 	for (T i = 1; i <= 10; i++) {
 		sum += i;
@@ -10,17 +10,24 @@ T subroutine() {
 	return sum;
 }
 
-int main() {
-
-	T R1 = subroutine();
-
-	T currentValue = 0;
-	for (T N = 2; N <= 9; N++) {
-		for (T M = 1; M <= 9; M++) {
-			currentValue = N * M;
-			printf("%d * %d = %d\n", N, M, currentValue);
+void subroutine_2(T* front, T* back, T* max, T* result) {
+	for (; *front <= *max; (*front)++) {
+		for (*back = 1; *back <= *max; (*back)++) {
+			*result = (*front) * (*back);
+			printf("%d * %d = %d\n", *front, *back, *result);
 		}
 	}
+}
+
+int main() {
+
+	T R1 = subroutine_1();
+
+	T R2 = 2;
+	T R3 = 1;
+	T R4 = 9;
+	T R5 = 0;
+	subroutine_2(&R2, &R3, &R4, &R5);
 
 	return 0;
 }
